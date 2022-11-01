7a5a9352e02732c4fc0a762dd3775ad75e3da193
YeetEeey <radu.g.paraian@gmail.com>
2022-11-01 20:34:43 +0000

    movement commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//canvas
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
@@ -6,19 +7,32 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.2
+const gravity = 0.7
 
+//sprite class
 class Sprite {
-    constructor({ position, velocity }) {
+    constructor({ position, velocity, color = 'red' }) {
         this.position = position
         this.velocity = velocity
+        this.width = 50
         this.height = 150
         this.lastKey
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50
+        }
+        this.color = color
+        this.isAttacking
     }
 
     draw() {
-        c.fillStyle = 'red'
+        c.fillStyle = this.color
         c.fillRect(this.position.x, this.position.y, 50, this.height)
+
+        //attack
+        c.fillStyle = 'green'
+        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
     }
 
     update() {
@@ -32,9 +46,17 @@ class Sprite {
         } else {
             this.velocity.y += gravity
         }
+
+    attack() {
+        this.isAttacking = true
+        setTimeout(() => {
+            this.isAttacking = false
+        }, 100)
+        };
     }
 }
 
+//const
 const player = new Sprite({
     position: {
         x: 0,
@@ -54,11 +76,13 @@ const enemy = new Sprite({
     velocity: {
         x: 0,
         y: 0
-    }
+    },
+    color: 'blue'
 })
 
 console.log(player)
 
+//keys
 const keys = {
     a: {
         pressed: false
@@ -79,10 +103,11 @@ const keys = {
         pressed: false
     }
 }
-let lastKey
 
+//movement
 function animate() {
     window.requestAnimationFrame(animate)
+
     //console.log('go')
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
@@ -93,37 +118,50 @@ function animate() {
     enemy.velocity.x = 0
 
     //player movement
-    if (keys.a.pressed && lastKey === 'a') {
-        player.velocity.x = -1
-    } else if (keys.d.pressed && lastKey === 'd') {
-        player.velocity.x = 1
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = -5
+    } else if (keys.d.pressed && player.lastKey === 'd') {
+        player.velocity.x = 5
     }
 
     //enemy movement
     if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
-        enemy.velocity.x = -1
+        enemy.velocity.x = -5
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
-        enemy.velocity.x = 1
+        enemy.velocity.x = 5
+    }
+
+    //detect collision
+    if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x && 
+        player.attackBox.position.x <= enemy.position.x + enemy.width &&
+        player.attackBox.position.y + player.attackBox.height >= enemy.position.y
+        && player.attackBox.position.y <= enemy.position.y + enemy.height &&
+        player.isAttacking) {
+        console.log('go');
     }
 }
 
 animate()
 
+//pressed keys
 window.addEventListener('keydown', (event) => {
-    console.group(event.key);
+    //console.group(event.key);
     switch (event.key) {
 
         //player keys
         case 'd':
             keys.d.pressed = true
-            lastKey = 'd'
+            player.lastKey = 'd'
             break
         case 'a':
             keys.a.pressed = true  
-            lastKey = 'a'
+            player.lastKey = 'a'
             break
         case 'w':
-            player.velocity.y = -10
+            player.velocity.y = -20
+            break
+        case ' ':
+            player.attack()
             break
 
         //enemy keys
@@ -136,12 +174,13 @@ window.addEventListener('keydown', (event) => {
             enemy.lastKey = 'ArrowLeft'
             break
         case 'ArrowUp':
-            enemy.velocity.y = -10
+            enemy.velocity.y = -20
             break
     }
     console.log(event.key)
 })
 
+//lifted keys
 window.addEventListener('keyup', (event) => {
     switch (event.key) {
 
